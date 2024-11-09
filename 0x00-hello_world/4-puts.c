0bd02f8ba6c466fd7c25f7e3451fe7ce094ebd4e
yassine <yelkadiri39@gmail.com>
2024-11-09 15:39:46 +0100

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Return: Alawys 0 (Success)
-*/
-
-int main(void)
+int main ()
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}    
