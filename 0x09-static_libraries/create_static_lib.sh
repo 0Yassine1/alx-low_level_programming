324ad67cbad446432fef5a93a67268360e548b6c
yassine <yelkadiri39@gmail.com>
2024-11-21 23:32:10 +0100

    s

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+# Creating a static library called liball.a from all .c files
+
+# Compile all .c files into .o object files
 gcc -c *.c
-ar rc liball.a *.o
+
+# Create a static library liball.a from the object files
+ar rcs liball.a *.o
+
+# Remove the object files after creating the library
+rm -f *.o
